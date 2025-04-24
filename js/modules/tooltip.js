d99e31e06ac36be138abca07e6459f957a4769e6
Gustavo <gustaaavox@gmail.com>
2025-04-24 20:19:54 -0300

    first commit

---
+++ js/modules/tooltip.js
@@ -0,0 +1,40 @@
+export default function initToolTip() {
+  const tools = document.querySelectorAll('[data-tooltip]')
+
+  tools.forEach((tool) => {
+    tool.addEventListener('mouseover', onMouseOver)
+  }) 
+
+  function onMouseOver(event) {
+    const tooltipBox = criarToolTipBox(this)
+
+    onMouseMove.tooltipBox = tooltipBox
+    this.addEventListener('mousemove', onMouseMove)
+
+    onMouseLeave.tooltipBox = tooltipBox
+    onMouseLeave.element = this
+    this.addEventListener('mouseleave', onMouseLeave)
+  }
+  const onMouseLeave = {
+    tootipBox: '',
+    element: '',
+    handleEvent() {
+    this.tooltipBox.remove()
+    this.element.removeEventListener('mouseleave', onMouseLeave)
+    },
+  }
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+  }
+  function criarToolTipBox(element) {
+    const tooltipBox = document.createElement('div')
+    const text = element.getAttribute('aria-label')
+    tooltipBox.classList.add('tooltip')
+    tooltipBox.innerText = text
+    document.body.appendChild(tooltipBox)
+    return tooltipBox
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import scrollSuave from "./modules/scrollSuave.js";
+import initAnimaScroll from "./modules/initAnimaScroll.js";
+import modal from "./modules/modal.js"
+import initToolTip from "./modules/tooltip.js"
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+modal()
+initTabNav()
+initAccordion()
+scrollSuave()
+initAnimaScroll()
+initToolTip()
+initDropdownMenu()
