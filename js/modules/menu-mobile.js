7d249a31453b4af6bd4dabacade65e6004d65696
Gustavo <gustaaavox@gmail.com>
2025-04-27 19:29:03 -0300

    chore:function initmenumobile exported properly

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,18 +1,22 @@
 import outsideClick from './outsideClick.js';
 
 export default function initMenuMobi() {
-}
-const menuBtn = document.querySelector('[data-menu="button"]')
-const menuList = document.querySelector('[data-menu="list"]')
-const eventos = ['click', 'touchstart']
 
-function openMenu(event) {
-  menuList.classList.add('active')
-  menuBtn.classList.add('active')
-  outsideClick(menuList, eventos, () => {
-    menuList.classList.remove('active')
-    menuBtn.classList.remove('active')
-  })
+  const menuBtn = document.querySelector('[data-menu="button"]')
+  const menuList = document.querySelector('[data-menu="list"]')
+  const eventos = ['click', 'touchstart']
+  if (menuBtn){
+    function openMenu(event) {
+      menuList.classList.add('active')
+      menuBtn.classList.add('active')
+      outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active')
+      menuBtn.classList.remove('active')
+      })
+    }
+    eventos.forEach(evento => menuBtn.addEventListener(evento, openMenu))
+  }
+  
 }
-eventos.forEach(evento => menuBtn.addEventListener(evento, openMenu))
+
 
