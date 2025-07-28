b69fe66a73a387881330968e5f79f58089db7842
Gustavo <gustaaavox@gmail.com>
2025-07-28 20:46:08 -0300

    refactor:refactored scrollsuave

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import initTabNav from "./modules/tabnav.js";
 import initAccordion from "./modules/accordion.js";
-import scrollSuave from "./modules/scrollSuave.js";
 import initAnimaScroll from "./modules/initAnimaScroll.js";
 import modal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
@@ -9,11 +8,14 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fectch-bitcoin.js";
+import ScrollSuave from "./modules/scrollSuave.js";
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 modal();
 initTabNav();
 initAccordion();
-scrollSuave();
 initAnimaScroll();
 initToolTip();
 initDropdownMenu();
