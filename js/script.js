55252ab96fed7bcf5c7a9a5b1b54aa1bbe107f29
Gustavo <gustaaavox@gmail.com>
2025-07-29 18:53:54 -0300

    chore: animaScroll position

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import initTabNav from "./modules/tabnav.js";
 import initAccordion from "./modules/accordion.js";
-import initAnimaScroll from "./modules/initAnimaScroll.js";
 import modal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,6 +8,7 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fectch-bitcoin.js";
 import ScrollSuave from "./modules/scrollSuave.js";
+import initAnimaScroll from "./modules/initAnimaScroll.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -16,10 +16,10 @@ scrollSuave.init();
 modal();
 initTabNav();
 initAccordion();
-initAnimaScroll();
 initToolTip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimaScroll();
