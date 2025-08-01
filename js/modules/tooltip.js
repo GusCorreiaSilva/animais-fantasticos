e6ba567b9cb31371b7e0e89f51b20601ace4d4a4
Gustavo <gustaaavox@gmail.com>
2025-08-01 14:24:23 -0300

    feat: tooltip now change position according to mouse position

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -8,15 +8,21 @@ export default class initToolTip {
     this.onMouseOver = this.onMouseOver.bind(this);
   }
 
+  //move a tip com base em seus estilos de acordo com a posição do mouse
   onMouseMove(event) {
-    this.tooltipBox.style.top = event.pageY + 20 + "px";
-    this.tooltipBox.style.left = event.pageX + 20 + "px";
+    this.tooltipBox.style.top = `${event.pageY + 20}px`;
+    if (event.pageX + 240 > window.innerWidth) {
+      this.tooltipBox.style.left = `${event.pageX - 190}px`;
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 20}px`;
+    }
   }
 
-  onMouseLeave(event) {
+  //remove a tooltip e os event de mouseMove e mouseÇeave
+  onMouseLeave({ currentTarget }) {
     this.tooltipBox.remove();
-    event.currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
-    event.currentTarget.removeEventListener("mousemove", this.onMouseMove);
+    currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
+    currentTarget.removeEventListener("mousemove", this.onMouseMove);
   }
   //cria a tooltip e coloca no body
   criarToolTipBox(element) {
@@ -27,19 +33,19 @@ export default class initToolTip {
     document.body.appendChild(tooltipBox);
     this.tooltipBox = tooltipBox;
   }
+
+  //cria a tooltip e cria os event mouseMove e mouseLeave ao target ao passar o mouse por cima do mapa
   onMouseOver(event) {
     // cria a tooltip e coloca numa propriedade
     this.criarToolTipBox(event.currentTarget);
-
     event.currentTarget.addEventListener("mousemove", this.onMouseMove);
-
     event.currentTarget.addEventListener("mouseleave", this.onMouseLeave);
   }
-
+  //add os event de mouseMove a cada tooltip do site
   addTooltipsEvent() {
     this.tools.forEach((tool) => {
       tool.addEventListener("mouseover", this.onMouseOver);
-    });
+    })
   }
   init() {
     if (this.tools.length) {
